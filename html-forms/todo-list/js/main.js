2a7870c21d9410e45eee48412206f4d28b903551
rodionov217 <henryshaw217@gmail.com>
2018-07-09 14:04:20 +0300

    first

---
+++ html-forms/todo-list/js/main.js
@@ -0,0 +1,15 @@
+const fieldset = document.getElementsByClassName('list-block')[0];
+const tasks = fieldset.querySelectorAll('input[type="checkbox"]');
+const output = fieldset.getElementsByTagName('output')[0];
+
+document.addEventListener('DOMContentLoaded', updateCounter);
+
+tasks.forEach(task => task.addEventListener('click', updateCounter));
+
+function updateCounter() { 
+  let done = Array.from(tasks).filter(task => task.checked);
+  if (done.length === tasks.length) { 
+    fieldset.classList.add('complete');
+  }
+  output.value = `${done.length} of ${tasks.length}`;
+}
