2477c2476f760011a48c1b35a3085d2e0c92739e
rodionov217 <henryshaw217@gmail.com>
2018-07-22 00:24:14 +0300

    first

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,26 @@
 'use strict';
+const counter = document.getElementById('counter');
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+
+function updateCounter() { 
+  counter.textContent = localStorage.counter;
+}
+
+increment.addEventListener('click', () => {
+  ++localStorage.counter;
+  updateCounter();
+});
+
+decrement.addEventListener('click', () => {
+  --localStorage.counter;
+  updateCounter();
+});
+
+reset.addEventListener('click', () => { 
+  localStorage.counter = 0;
+  updateCounter();
+})
+
+document.addEventListener('DOMContentLoaded', updateCounter);
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const xhr = new XMLHttpRequest();
+const signInForm = document.getElementsByClassName('sign-in-htm')[0];
+const outputIn = signInForm.getElementsByTagName('output')[0];
+const button = signInForm.getElementsByClassName('button')[0];
+
+button.addEventListener('click', () => {
+  event.preventDefault();
+  xhr.open('POST', 'https://neto-api.herokuapp.com/signin');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  const body = {
+    email: signInForm.querySelector('#email').value,
+    password: signInForm.querySelector('#pass').value
+  };
+  xhr.send(JSON.stringify(body));
+});
+
+xhr.addEventListener('load', () => {
+  const response = JSON.parse(xhr.response);
+  outputIn.innerText = response.error ? response.message : `Пользователь ${response.name} успешно авторизирован`;
+});
+
+const xhr2 = new XMLHttpRequest();
+const signUpForm = document.getElementsByClassName('sign-up-htm')[0];
+const outputUp = signUpForm.getElementsByTagName('output')[0];
+const button2 = signUpForm.getElementsByClassName('button')[0];
+
+button2.addEventListener('click', () => {
+  event.preventDefault();
+  xhr2.open('POST', 'https://neto-api.herokuapp.com/signup');
+  xhr2.setRequestHeader('Content-Type', 'application/json');
+  const body = {
+    email: signUpForm.querySelector('#email').value,
+    password: signUpForm.querySelector('#pass').value,
+    passwordcopy: signUpForm.querySelectorAll('#pass')[1].value,
+    name: signUpForm.querySelectorAll('#pass')[2].value
+  };
+  xhr2.send(JSON.stringify(body));
+});
+
+xhr2.addEventListener('load', () => {
+  const response = JSON.parse(xhr2.response);
+  outputUp.innerText = response.error ? response.message : `Пользователь ${response.name} успешно зарегистрирован`;
+});
