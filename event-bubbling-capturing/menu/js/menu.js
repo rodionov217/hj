9bfa7135503c016f3dde33a2967686aa8b64a898
rodionov217 <rodionova217@gmail.com>
2018-08-14 18:27:13 +0300

    исправлено схлапывание меню

--- event-bubbling-capturing/menu/js/menu.js
+++ event-bubbling-capturing/menu/js/menu.js
@@ -1,20 +1,21 @@
 'use strict';
 
 function toggleMenu(event) {
-  if (event.target.classList.contains('show')) {
+  event.preventDefault();
+  if (event.currentTarget.classList.contains('show')) {
     event.currentTarget.classList.remove('show');
     event.currentTarget.classList.add('hide');
   } else {
     event.currentTarget.classList.add('show');
     event.currentTarget.classList.remove('hide');
   }
-  
 }
 
 function openLink(event) {
   event.preventDefault();
   console.log(event.target.textContent);
-  event.stopPropagation();
+  event.target.closest('.dropdown').classList.add('hide');
+  event.target.closest('.dropdown').classList.remove('show');
 }
 
 function init(node) {
@@ -25,7 +26,7 @@ function initLink(node) {
   if (node.dataset.toggle) {
     return;
   }
-  node.addEventListener('click', openLink, true);
+  node.addEventListener('click', openLink);
 }
 
 Array
